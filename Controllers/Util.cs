94a2ce65a2d0324a7923d1e7bce9e8541a09fbbe
lucasIEFP <luc.gabriel.2011@gmail.com>
2026-01-06 14:39:25 +0000

    Add project files.

---
+++ Controllers/Util.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lixooo.Controllers
+{
+    public class Util : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
